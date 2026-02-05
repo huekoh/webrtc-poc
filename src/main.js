5b92f8e4024d520972b19b86e043af78d19b7e92
huekoh <huexkoh@gmail.com>
2026-02-05 19:34:47 +0800

    Create local and remote media retrieval function

--- src/main.js
+++ src/main.js
@@ -1,24 +1,76 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  updateDoc,
+  onSnapshot,
+  addDoc,
+} from "firebase/firestore";
 
-setupCounter(document.querySelector('#counter'))
+const firebaseConfig = {
+  apiKey: "AIzaSyBvn-39AR0QVBWc1m5mDYlIzO_PLrcfKBw",
+  authDomain: "webrtc-poc-4a2b5.firebaseapp.com",
+  projectId: "webrtc-poc-4a2b5",
+  storageBucket: "webrtc-poc-4a2b5.firebasestorage.app",
+  messagingSenderId: "893447120105",
+  appId: "1:893447120105:web:77b24add8837601d1a3c78",
+  measurementId: "G-ZHTEY1X8P7",
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+// global state
+const pc = new RTCPeerConnection(servers);
+let localStream = null;
+let remoteStream = null;
+
+// html elements
+const webcamButton = document.getElementById("webcamButton");
+const webcamVideo = document.getElementById("webcamVideo");
+const callButton = document.getElementById("callButton");
+const callInput = document.getElementById("callInput");
+const answerButton = document.getElementById("answerButton");
+const remoteVideo = document.getElementById("remoteVideo");
+const hangupButton = document.getElementById("hangupButton");
+
+// set up media sources
+webcamButton.onclick = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  });
+  remoteStream = new MediaStream();
+
+  // push tracks for local stream to peer connection
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+
+  // pull tracks from remote stream and add to video stream
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  webcamVideo.srcObject = localStream;
+  remoteVideo.srcObject = remoteStream;
+
+  callButton.disabled = false;
+  answerButton.disabled = false;
+  webcamButton.disabled = true;
+};
